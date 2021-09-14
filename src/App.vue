084818ddbebd56cf62a023d8bff3338faa09a3f8
yiğitcan Öztürk <yozturk037@gmail.com>
2021-09-14 08:57:22 +0300

    API logic download

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
   <div id="app">
-    
+    <div class="container">
+      <h1 class="text-center">Vue-Translator</h1>
+      <h5 class="text-center">Vue.js ile Translator</h5>
+      <br />
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <searchTranslate />
+        </div>
+      </div>
+      <div class="well">
+        <h3>Arama Geçmişi</h3>
+        <oldSearch />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import searchTranslate from "./components/searchTranslate.vue";
+import oldSearch from "./components/oldSearch.vue";
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  components: {
+    searchTranslate,
+    oldSearch
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -25,7 +44,8 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
